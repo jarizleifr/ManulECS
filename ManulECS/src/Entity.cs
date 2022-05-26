bb0f4b7857e138a391152964ae9e03de99e7c982
Jarizleifr <antti.joutsi@gmail.com>
2022-05-26 16:09:23 +0300

    Cleanup code, add comments

--- ManulECS/src/Entity.cs
+++ ManulECS/src/Entity.cs
@@ -1,36 +1,35 @@
 using System;
-using System.Runtime.CompilerServices;
 
 namespace ManulECS {
+  /* Entity is an opaque wrapper around an unsigned integer, where the lowest 3 bytes represent the
+   * Id and the highest byte represents the Version of the Entity.
+   */
+  /// <summary>An Entity, which can own Components and/or Tags.</summary>
   public readonly struct Entity : IEquatable<Entity> {
     public static readonly Entity NULL_ENTITY = new(NULL_ID, 0);
     public const uint NULL_ID = 0xFFFFFF;
 
     internal readonly uint uuid;
 
-    internal uint Id {
-      [MethodImpl(MethodImplOptions.AggressiveInlining)]
-      get => uuid >> 8;
-    }
+    /* The compiler seems to have some trouble inlining this property, I've omitted the MethodImpl
+     * here, as it simply does nothing. We could have the Id and the Version as separate fields,
+     * effectively using a 64-bit Entity instead, but the padding it causes in the Entity array makes
+     * the final result slower, than just accepting the overhead from this shift.
+     */
+    internal uint Id => uuid >> 8;
     internal byte Version => (byte)uuid;
 
     internal Entity(uint id, byte version) => uuid = id << 8 | version;
     internal Entity(uint uuid) => this.uuid = uuid;
 
-    internal void Deconstruct(out uint id, out byte version) {
-      id = Id; version = Version;
-    }
+    internal void Deconstruct(out uint id, out byte version) => (id, version) = (Id, Version);
 
     public bool Equals(Entity entity) => uuid == entity.uuid;
 
     public static bool operator ==(Entity left, Entity right) => left.Equals(right);
     public static bool operator !=(Entity left, Entity right) => !left.Equals(right);
 
-    public override string ToString() => $"{Id}:{Version}";
-
-    public override bool Equals(object obj) =>
-      obj is Entity entity && Equals(entity);
-
+    public override bool Equals(object obj) => obj is Entity entity && Equals(entity);
     public override int GetHashCode() => (int)uuid;
   }
 
@@ -40,9 +39,6 @@ namespace ManulECS {
 
     internal EntityHandle(World world, Entity entity) =>
       (this.world, this.entity) = (world, entity);
-
-    public bool Has<T>() where T : struct, IBaseComponent =>
-      world.Has<T>(entity);
 
     public EntityHandle Tag<T>() where T : struct, ITag {
       world.Tag<T>(entity);
@@ -63,11 +59,6 @@ namespace ManulECS {
       world.Remove<T>(entity);
       return this;
     }
-
-    public void Clone(int count = 1) => world.Clone(entity, count);
-
-    public EntityHandle If(bool statement, Func<EntityHandle, EntityHandle> callback) =>
-      statement ? callback.Invoke(this) : this;
 
     public static implicit operator Entity(EntityHandle handle) => handle.entity;
   }
